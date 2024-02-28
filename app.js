a98642dc8e300f03344463e280ce5d799c0ae107
aslah <aslahcholasseri@gmail.com>
2024-02-28 09:49:51 -0500

    Initial commit

--- app.js
+++ app.js
@@ -1,24 +1,72 @@
-/**
- * 
- * <div id='parent>
- * <div id=child>
- * <h1> hello world</h1>
- * 
- * </div>
- * </div>
- * 
- * 
- */
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <div className="logo-container"> 
+                <img className="logo" src='https://is1-ssl.mzstatic.com/image/thumb/Purple116/v4/c8/26/95/c82695e7-bda5-c9fc-3a23-6f3b8fdb302a/AppIcon-0-1x_U007emarketing-0-7-0-85-220.png/1200x630wa.png' alt="Logo" />
+            </div>
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About Us</li >
+                    <li>Cart</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
 
 
-const parent = React.createElement("div",{id:"parent"},[React.createElement('div',{id:"child"},React.createElement("h1",{},'hello world')),React.createElement('div',{id:"child"},React.createElement("h1",{},'hello world'))]);
+const styleCard = {
+    backgroundColor :"orange",
+}
+ 
 
-console.log(parent)
+const Restocard = (props)=>{
+    console.log(props)
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(parent)
+    return(
+        <div className="restocard" style={styleCard}>
+             <img className='res-img' alt='logo' src="https://w.forfun.com/fetch/42/42a91f276a7e9a7e5f1b60d811a28daa.jpeg" />
+             <h3>{props.resName}</h3>
+              <h2>{props.cusine }</h2>
+              <h3>4.3</h3>
+       
+       
+        </div>
+    )
+}
 
+const Body = () => {
+    return(
+        <div className="body">
 
-// const heading = React.createElement("h1", {id:'heading'}, "hello world from React");
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(heading);
+            <div className="search">
+             search
+            </div>
+            <div className="res-container" >
+
+      <Restocard resName='burger' cusine='burger , fast food' />
+      <Restocard resName='biriyani'  cusine='burger , fast food'   />
+    
+
+            </div>
+        </div>
+    )
+}
+const AppLayout = () => {
+    return (
+        <div className="app">
+            <Header />
+            <Body />
+          
+
+        </div>
+    )
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+
+root.render(<AppLayout />)
